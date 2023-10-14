99af9d55a32e551f0eb0cd00506d7eb987684d0e
Kevin Newton <kddnewton@gmail.com>
2023-10-14 14:58:26 -0400

    Ensure we have an object for each instruction

--- test/yarv/compile_test.rb
+++ test/yarv/compile_test.rb
@@ -4,10 +4,23 @@ require_relative "test_helper"
 
 module YARV
   class CompileTest < Minitest::Test
+    # Test that we can compile every Ruby file in the lib directory of our
+    # current Ruby version.
     Dir[File.join(RbConfig::CONFIG["libdir"], "**", "*.rb")].each do |filepath|
       define_method("test_#{filepath}") do
         assert_compiles(filepath)
       end
+    end
+
+    # Test that we have an instruction class for every instruction in the
+    # current Ruby version.
+    def test_instruction_names
+      expected = RubyVM::INSTRUCTION_NAMES.grep_v(/^trace/).map { |name| name.delete("_").downcase }.sort
+
+      actual = ObjectSpace.each_object(YARV::Instruction.singleton_class).map { |cls| cls.name.split("::").last.downcase }.sort
+      known = %w[invokebuiltin optinvokebuiltindelegate optinvokebuiltindelegateleave optreverse]
+
+      assert_empty(expected - actual - known)
     end
 
     private
